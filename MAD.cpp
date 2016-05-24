9d486585c7aec47a3f80f249f20126c9a1f276e5
Vanda Azevedo <vanda.fba@gmail.com>
2016-05-24 18:51:56 +0100

    Vanda adding some changes.

--- MAD.cpp
+++ MAD.cpp
@@ -1,13 +1,9 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-
 #define COOP 0
 #define CHEAT 1
 
-int choice, turns=0, points=0, enemy=0;
-double probC=0, probT=0;
-bool over=false;
+int choice, turn=0, points=0, enemy=0, combo1=0, combo2=0, combo3=0, combo4=0;
 
 int main(){
 	cout << "Choose the strategy you want to test:" << endl;
@@ -16,7 +12,10 @@ int main(){
 	cout << "3- Random 50-50" << endl;
 	cout << "4- Começar por cooperar e depois jogar sempre o que o outro jogou" << endl;
 	cin >> choice;
-	start();
+	if(choice!=1 || choice!=2 || choice!=3 || choice!=4)
+		cout << "Input errado." << endl;
+	else
+		start();
 
 return 0;
 }
@@ -25,15 +24,85 @@ return 0;
 // TC - 5,0
 // TT - 1,1
 void start(){
-	while(over==false){
-		if(rand<min)
-			over=true;
-		else{
-			if(choice==1){
-
-			}
+	string strategy;
+	int player1, player2;
+	bool over = false;
+	while(!over){
+		over=endGame();
+		turn++;
+		if(choice == 1){
+			player1=strategy();
+			player2=cooperar();
+			sums(player1, player2);
+		}
+		else if(choice == 2){
+			player1=strategy();
+			player2=trair();
+			sums(player1, player2);
 		}
 	}
+	if(over){
+		if(choice==1)
+			strategy = "cooperar sempre";
+		else if(choice==2)
+			strategy = "trair sempre";
+		else if(choice==3)
+			strategy = "random";
+		else
+			strategy = "começar por cooperar e depois jogar sempre o que o outro jogou";
+		cout << "Pontos da nova estratégia: " << points << endl;
+		cout << "Pontos da estratégia " << strategy << ": " << enemy << endl;
+	}
+}
+
+int strategy(){
+	int c;
+	if(turn <= 7)
+		c=COOP;
+	else if(turn > 7 && turn <= 10)
+		c=CHEAT;
+	else{
+		if(combo1>combo3 && combo2>combo4)
+			c=COOP;
+		else if(combo3>combo1 && combo3>combo4)
+			c=CHEAT;
+		else
+			c=CHEAT;
+	}
+	return c;
+}
+
+void sums(int player1, int player2){
+	if(player1==COOP && player2==COOP){
+		points+=3;
+		enemy+=3;
+		combo1+=3;
+	}
+	else if(player1==COOP && player2==CHEAT){
+		enemy+=5;
+		combo2=0;
+	}
+	else if(player1==CHEAT && player2==COOP){
+		points+=5;
+		combo3+=5;
+	}
+	else if(player1==CHEAT && player2==CHEAT){
+		points+=1;
+		enemy+=1;
+		combo4+=1;
+	}
+}
+
+int cooperar(){
+	playC++;
+	probC=playC/turns;
+	return COOP;
+}
+
+int trair(){
+	probT++;
+	probT=playT/turns;
+	return CHEAT;
 }
 
 bool endGame() {
